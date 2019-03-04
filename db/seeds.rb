daaa5f39f3e35fa73bf5488fb0c15cd70e800524
Ben Pham <benjamin.pham@gmail.com>
2019-03-04 16:39:00 +0000

    Added Faker to gemfile and started populating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+require 'faker'
+
+puts "starting to seed..."
+
+Registration.destroy_all
+Section.destroy_all
+User.destroy_all
+
+students = (1..50).each_with_object({}) do |i, student|
+  student[i] = User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , password: "secret")
+end
+
+teachers = (1..5).each_with_object({}) do |i, teacher|
+  teacher[i] = User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , password: "secret", is_teacher: true)
+end
+
+sections = (1..10).each_with_object({}) do |i, section|
+  section[i] = Section.new(name: ('A'..'Z').to_a[i-1])
+end
+
+students.each_with_index do |student, index|
+  a = (1..10).to_a.shuffle
+  (1..3).to_a.sample.times do
+    registration = Registration.new
+    registration.user = students[index+1]
+    registration.section = sections[a.pop]
+    registration.save
+  end
+end
+
+puts "completed seeding..."
