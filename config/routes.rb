307ec3df382c052de6d173b8ca86f23a4da895e6
Ben Pham <benjamin.pham@gmail.com>
2019-03-04 19:18:20 +0000

    Created curriculum and decks routes controllers policies views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :curriculums, only [:show] do
-    resources :decks, only [:index, :show]
+  resources :curriculums, only:[:show] do
+    resources :decks, only:[:index, :show]
   end
 end
