f9ca944358f14bfa9da92a10a8a719409e7f940e
Izzy Weber <izzyweber@gmail.com>
2019-03-05 12:15:43 +0000

    routes added for deks and cards

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,5 @@
 class DecksController < ApplicationController
-  before_action :set_deck, only: [:show]
+  before_action :set_deck, only: [:show, :edit]
 
   def index
     @decks = policy_scope(Deck).joins(:curriculum).where("decks.curriculum_id = #{params[:curriculum_id]}")
@@ -7,6 +7,15 @@ class DecksController < ApplicationController
 
   def show
     authorize @deck
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :curriculums, only:[:show] do
-    resources :decks, only:[:index, :show]
+    resources :sections, only: [:show] do
+      resources :decks, only:[:index, :show, :new, :create, :edit] do
+        resources :cards, only[:index, :new, :create, :edit]
+      end
+    end
   end
 end
