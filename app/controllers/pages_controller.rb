559913461f13192421011c7a5fa46ce2baa1779c
Jonny Gates <jonny.gates@outlook.com>
2019-03-06 11:44:58 +0000

    Created curriculums scope policy to retrive users own curriculums

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user.is_teacher
+
+    else
+      #student stuff
+    end
   end
 end
