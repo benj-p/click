5459973eabbe08ed24b11091a5f4ee4ffa6c498f
Ben Pham <benjamin.pham@gmail.com>
2019-03-06 19:39:14 +0000

    Added javascript to find parent width

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+const toggleFixed = () => {
+    var parentwidth = $(".question-container").width();
+    $(".progress").width(parentwidth).css("position", "fixed").css("bottom", "16px");;
+}
+
+if (window.location.pathname.includes('/takedeck')) {
+  toggleFixed();
+}
