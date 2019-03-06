777a29ff5f97f33ac27a3819654b4e89fd79ed73
Izzy Weber <izzyweber@gmail.com>
2019-03-06 10:37:59 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :curriculums, only:[:show] do
-    resources :decks, only:[:edit, :update, :new, :create] do
-      resources :cards, only:[:edit, :update, :new, :create]
-    end
-    resources :sections, only: [:show] do
-      resources :decks, only:[:index, :show] do
-        resources :cards, only:[:index, :show]
-      end
+    resources :decks, only:[:index, :edit, :update, :new, :create] do
+      resources :cards, only:[:edit, :update, :new, :create, :index, :show]
     end
   end
-
   resources :sections, only: :show do
     resources :decks, only: :show
   end
