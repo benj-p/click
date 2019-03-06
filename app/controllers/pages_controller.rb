430e307fe2f913bd3fae5c26f557ff3760c68b43
Jonny Gates <jonny.gates@outlook.com>
2019-03-06 13:09:10 +0000

    Redirect to curriculums upon login, clean up student & teacher dashboards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if current_user.is_teacher
-
-    else
-      #student stuff
-    end
   end
 end
