d6be5c4585a1489bf1cb82db8d1a2108507ba1ca
Ben Pham <benjamin.pham@gmail.com>
2019-03-06 16:05:30 +0000

    Created deck cards

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,10 +4,10 @@ class UserPolicy < ApplicationPolicy
   end
 
   def takedeck?
-    @record == @user
+    @record == @user && @user.is_teacher == false
   end
 
   def decksummary?
-    @record == @user
+    @record == @user && @user.is_teacher == false
   end
 end
