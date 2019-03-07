3a8571c07e3c031989584bfc9c153264239e81d3
Jonny Gates <jonny.gates@outlook.com>
2019-03-07 15:49:57 +0000

    Link decks to deck results

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -6,6 +6,16 @@ class Deck < ApplicationRecord
   validates :name, presence: true
 
   def deck_results(section)
+    attempts = deck_attempts(section)
+    total = attempts.count.to_f
+    {
+      correct: (attempts.count { |attempt| attempt.response == "Correct" } / total) * 100,
+      incorrect: (attempts.count { |attempt| attempt.response == "Incorrect" } / total) * 100,
+      unsure: (attempts.count { |attempt| attempt.response == "I don't know" } / total) * 100
+    }
+  end
+
+  def deck_attempts(section)
     attempts = []
     self.curriculum.sections.where(id: section.id).first.users.each do |user|
       user.attempts.each { |attempt| attempts << attempt }
