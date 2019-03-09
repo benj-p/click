dfa5d017ab42ba841b430514d2b3f7e66ba6f2bd
Jonny Gates <jonny.gates@outlook.com>
2019-03-09 13:07:55 +0000

    Add validation to to feed event for about user

--- app/models/feed_event.rb
+++ app/models/feed_event.rb
@@ -1,6 +1,6 @@
 class FeedEvent < ApplicationRecord
   belongs_to :user
-  belongs_to :about_user, :class_name => 'User', :foreign_key => 'about_user_id'
+  belongs_to :about_user, :class_name => 'User', :foreign_key => 'about_user_id', optional: true
 
 
   belongs_to :deck, optional: true
