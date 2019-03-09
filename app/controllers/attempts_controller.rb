fa38b622e2d7b6c866ede09b457df614c936df78
Jonny Gates <jonny.gates@outlook.com>
2019-03-09 10:52:48 +0000

    Move completed_deck method to attempts controller

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -10,13 +10,42 @@ class AttemptsController < ApplicationController
     if @question < @deck.cards.count
       redirect_to takedeck_user_path(@user, deck: @deck.id, question: @question) if @attempt.save
     else
+      completed_deck(@deck)
       redirect_to decksummary_user_path(@user, deck: @deck) if @attempt.save
     end
   end
+
+
 
   private
 
   def attempt_params
     params.permit(:response, :card_id)
+  end
+
+  def completed_deck(deck)
+    teacher = deck.curriculum.user
+    attempts = deck.attempts.where(user: current_user)
+    correct = calculate_percent(attempts.where(response: "Correct"), attempts.count)
+    incorrect = calculate_percent(attempts.where(response: "Incorrect"), attempts.count)
+    unsure = calculate_percent(attempts.where(response: "I don't know"), attempts.count)
+
+    if correct > 75
+      feed_event = FeedEvent.new(user: teacher, type: EventType.first)
+      feed_event.title = "#{ current_user.first_name } completed #{ deck.name } and scored #{ correct }%! Don't forget to say well done!"
+      feed_event.save
+    elsif incorrect > 75
+      feed_event = FeedEvent.new(user: teacher, type: EventType.first)
+      feed_event.title = "#{ current_user.first_name } completed #{ deck.name } and scored #{ correct }%! Don't forget to follow up!"
+      feed_event.save
+    elsif unsure > 75
+      feed_event = FeedEvent.new(user: teacher, type: EventType.first)
+      feed_event.title = "#{ current_user.first_name } completed #{ deck.name } and answered #{ unsure }% as unsure. Make sure you check in."
+      feed_event.save
+    end
+  end
+
+  def calculate_percent(attempts, total)
+    (attempts / total) * 100
   end
 end
